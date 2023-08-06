c0a3937596a25243f91a6d16dd92e5a963fb3101
Juan Miguel Acosta <acostaojuanmi@gmail.com>
2023-08-06 17:53:18 +0200

    ctes

--- constantes.js
+++ constantes.js
@@ -1,6 +1,7 @@
 // DEFINICIÓN DE CONSTANTES
 
-export const APIKEY = "";//Pon tu APIKEY de openweathermap
+export const APIKEY = "db855f64e2ab1800dd59a76414f8824f";//Pon tu APIKEY de openweathermap
 export const APIURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 export const CITY = document.querySelector("#buscador input");
 export const BTN = document.querySelector("#buscador #btn");
+export const ICON = document.querySelector("#weather-icon");
