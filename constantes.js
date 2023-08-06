a601bde3f240e7a429d08babcf92fe1d766d9ca5
Juan Miguel Acosta <acostaojuanmi@gmail.com>
2023-08-06 16:56:34 +0200

    Actualización de ctes.

--- constantes.js
+++ constantes.js
@@ -1,6 +1,6 @@
 // DEFINICIÓN DE CONSTANTES
 
-export const APIKEY = "db855f64e2ab1800dd59a76414f8824f";
+export const APIKEY = "";//Pon tu APIKEY de openweathermap
 export const APIURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 export const CITY = document.querySelector("#buscador input");
 export const BTN = document.querySelector("#buscador #btn");
