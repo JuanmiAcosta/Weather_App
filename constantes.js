54c0c7f4edbef6c6cdf7950b4ce40f68693c66a3
Juan Miguel Acosta <acostaojuanmi@gmail.com>
2023-08-06 22:51:58 +0200

    Weather app finalizada.

--- constantes.js
+++ constantes.js
@@ -1,10 +1,7 @@
 // DEFINICIÓN DE CONSTANTES
 
-export const APIKEY = "";//Pon tu APIKEY de openweathermap
+export const APIKEY = ""; //Pon tu APIKEY de openweathermap
 export const APIURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 export const CITY = document.querySelector("#buscador input");
 export const BTN = document.querySelector("#buscador #btn");
 export const ICON = document.querySelector("#weather-icon");
-
-
-export const ICON_POLLEN = document.querySelector("#weather-icon");
