943d12bb8d566f8a68b3ec56b24084e8fb7532e4
Juan Miguel Acosta <acostaojuanmi@gmail.com>
2023-08-06 16:32:54 +0200

    Borrado APIKEY

--- constantes.js
+++ constantes.js
@@ -1,5 +1,6 @@
 // DEFINICIÓN DE CONSTANTES
 
-export const APIKEY = "db855f64e2ab1800dd59a76414f8824f";
+export const APIKEY = ""; // API KEY DE OPENWEATHERMAP
 export const APIURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-
+export const CITY = document.querySelector("#buscador input");
+export const BTN = document.querySelector("#buscador button");
