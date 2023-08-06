f34ec32906b9ddcf7be52f4dcc64e7d7210b1842
Juan Miguel Acosta <acostaojuanmi@gmail.com>
2023-08-06 18:04:03 +0200

    Revocada clave anterior.

--- constantes.js
+++ constantes.js
@@ -1,7 +1,10 @@
 // DEFINICIÓN DE CONSTANTES
 
-export const APIKEY = "db855f64e2ab1800dd59a76414f8824f";//Pon tu APIKEY de openweathermap
+export const APIKEY = "";//Pon tu APIKEY de openweathermap
 export const APIURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 export const CITY = document.querySelector("#buscador input");
 export const BTN = document.querySelector("#buscador #btn");
 export const ICON = document.querySelector("#weather-icon");
+
+
+export const ICON_POLLEN = document.querySelector("#weather-icon");
